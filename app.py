91c1479af5fd49c7a61e36404921289575aec190
a368148-alt <a368148@uach.mx>
2025-10-13 09:54:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Mi aplicación para calcular el área de un círculo 🤓")
 import math
-radio = st.slider("Selecciona el radio", 0.0, 10.0, 5.0)
+radio = st.slider("Selecciona el radio", 0.0, 100.0, 5.0)
 area = math.pi*radio**2
 st.write(f"El área del cículo con radio {radio} es:{area: .2f}")
